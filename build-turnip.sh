abfb07b77a10f9c01bb32b70dc4487fc1fd3d54a
Tornado6896 <tornado6896@gmail.com>
2026-03-31 15:42:16 +0300

    Update build-turnip.sh

--- build-turnip.sh
+++ build-turnip.sh
@@ -12,9 +12,7 @@ sdkver="34"
 
 # Define Mesa version and download URL
 mesadir="mesa-mesa-26.0.3"
-#mesaver="https://gitlab.freedesktop.org/mesa/mesa/-/archive/mesa-26.0.3/mesa-mesa-26.0.3.zip?ref_type=tags"
-#mesaver="https://github.com/whitebelyash/mesa-tu8"
-mesasrc="https://github.com/Tornado6896/mesa_26_03.git"
+mesaver="https://github.com/Tornado6896/mesa_26_03.git"
 
 # Define working directories
 workdir="$(pwd)/turnip_workdir"         # Base directory for all operations
@@ -74,13 +72,12 @@ unzip "$ndkdir".zip &> /dev/null
 # Download Mesa source
 echo "Downloading Latest Mesa source ..." $'\n'
 #curl $mesaver --output "$mesadir".zip &> /dev/null
+git clone $mesaver &> /dev/null
 
-#clear
+clear
 
-#echo "Extracting Mesa source..." $'\n'
+echo "Extracting Mesa source..." $'\n'
 #unzip "$mesadir".zip &> /dev/null
-#git clone $mesasrc $mesadir &> /dev/null
-curl $mesasrc $mesadir &> /dev/null
 cd $mesadir
 
 # Set NDK Clang bin directory
